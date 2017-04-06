cdc6a1f0d9d01dad5f540f2aa4e2bf08a35c4c3a
xiaoxijin <xiaoxijin23@126.com>
2017-04-06 19:43:50 +0800

    依赖pcm_local

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,12 @@
-FROM registry.cn-shenzhen.aliyuncs.com/xiaoxijin/php7.1:latest
+FROM registry.cn-shenzhen.aliyuncs.com/xiaoxijin/pcm_local:latest
 
 Maintainer Xijin Xiao (http://github.com/xiaoxijin/)
 
-###########################
-#RUN echo "http://nl.alpinelinux.org/alpine/latest-stable/main" > /etc/apk/repositories
-#RUN echo "http://nl.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories
-#RUN echo "nameserver 8.8.8.8" >> /etc/resolv.conf && apk update && apk upgrade
-#RUN apk add php7
-#RUN apk add php7-opcache
-#RUN ln -fs /usr/bin/php7 /usr/bin/php
-
-########################### build swoole.so and copy to /root/
+ADD composer-setup.php /root/
 ADD build_extend.sh /root/
 
 RUN sh /root/build_extend.sh
 
-ADD php.ini /etc/php7.1/
 
-ADD ext/* /etc/php7.1/conf.d/
+
 
--- build_extend.sh
+++ build_extend.sh
@@ -1,37 +1,13 @@
-#echo "http://nl.alpinelinux.org/alpine/latest-stable/main" > /etc/apk/repositories
-#echo "http://nl.alpinelinux.org/alpine/edge/testing/" >> /etc/apk/repositories
-#echo "nameserver 8.8.8.8" >> /etc/resolv.conf 
-
-apk update && apk upgrade
-
-apk add php7.1-opcache \
-php7.1-session \
-php7.1-json \
-php7.1-iconv \
-php7.1-mysqli \
-php7.1-posix
-
-
+cd /root/
 apk add autoconf build-base linux-headers \
 libaio-dev \
 zlib-dev \
 pcre-dev \
-php7.1-dev \
-php7.1-pecl
+php7-dev \
+php7-pecl
 
 
-ln -s /usr/bin/php-config7.1 /usr/bin/php-config \
-&& ln -s /usr/bin/phpize7.1 /usr/bin/phpize \
-&& ln -s /usr/bin/pecl7.1 /usr/bin/pecl \
-#sed -i "s/struct sigaction {/#ifndef __sighandler_t \ntypedef void (*__sighandler_t)(int);\n#endif\nstruct sigaction\n{/g" /usr/include/signal.h \
-#&& sed -i "s/union {void (*sa_handler)(int)/__sighandler_t sa_handler/g" /usr/include/signal.h \
-#&& sed -i "s/ -n / /" `which pecl` \
-pecl install swoole \
-&& pecl install yaconf \
-&& pecl install channel://pecl.php.net/yac-2.0.1 \
-&& pecl install xdebug \
-&& pecl install ds \
-&& pecl install inotify \
-&& pecl install msgpack \
-&& pecl install yaf \
-&& pecl install redis
+
+php -r "if (hash_file('SHA384', './composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php ./composer-setup.php
+php -r "unlink('./composer-setup.php');"
