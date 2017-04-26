dc99a3bc0a8c317175d036b4a4a2661bc789124f
xiaoxijin <xiaoxijin23@126.com>
2017-04-26 12:58:22 +0800

    去掉composer

--- build_extend.sh
+++ build_extend.sh
@@ -5,11 +5,3 @@ pcre-dev \
 php7-dev \
 php7-pecl \
 git
-
-
-php -r "if (hash_file('SHA384', '/work/composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php  ${WORK_DIR}omposer-setup.php
-php -r "unlink('/work/composer-setup.php');"
-mv ./composer.phar /usr/bin/composer
-
-composer require "twig/twig:^2.3"
